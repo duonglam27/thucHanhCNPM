18a8903b1412f671a6de7a734826eaaad1fb07fc
LamDuong27 <lamdnb27@gmail.com>
2024-11-17 00:30:25 +0700

    baoLam them loc theo loai sp

--- saleapp/app/dao.py
+++ saleapp/app/dao.py
@@ -7,10 +7,12 @@ def load_categories():
     return Category.query.order_by('id').all()
 
 
-def load_products(kw=None):
-    products=Product.query.order_by('category_id')
+def load_products(kw=None,cate_id=None,page=1):
+
+    query=Product.query
 
     if kw:
-        products=products.filter(Product.name.contains(kw))
-
-    return products.all()
+        query=query.filter(Product.name.contains(kw))
+    if cate_id:
+        query=query.filter(Product.category_id==cate_id)
+    return query.all()
--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -7,8 +7,10 @@ from app import app
 def index():
     cates=dao.load_categories()
 
+    cate_id=request.args.get('category_id')
+    page=request.args.get('page',1)
     kw=request.args.get('kw')
-    prods=dao.load_products(kw)
+    prods=dao.load_products(kw=kw,cate_id=cate_id,page=int(page))
     return render_template('index.html', categories=cates, products=prods)
 
 if __name__=='__main__':
